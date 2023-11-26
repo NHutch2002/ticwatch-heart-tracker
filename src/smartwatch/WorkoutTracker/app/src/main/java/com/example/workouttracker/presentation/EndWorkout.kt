6d9be91d609d9154a08368d84e74103a75ed5f55
Nathan Hutchison <nathanhutchison02@gmail.com>
2023-11-26 22:03:45 +0000

    Started work on sample graph, needs polishing on design, working on issue #19

--- src/smartwatch/WorkoutTracker/app/src/main/java/com/example/workouttracker/presentation/EndWorkout.kt
+++ src/smartwatch/WorkoutTracker/app/src/main/java/com/example/workouttracker/presentation/EndWorkout.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
@@ -24,17 +25,24 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.Text
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
 
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun EndWorkoutPage(navController: NavController, maxHeartRate: Float) {
-    val pagerState = rememberPagerState { 2 } // Replace 2 with your actual page count
+    val pagerState = rememberPagerState { 3 } // Replace 2 with your actual page count
 
     val context = LocalContext.current
     val viewModel: HeartRateMonitorViewModel = viewModel()
@@ -49,7 +57,8 @@ fun EndWorkoutPage(navController: NavController, maxHeartRate: Float) {
         HorizontalPager(state = pagerState) { page ->
             when (page) {
                 0 -> HRRPage(navController, maxHeartRate, viewModel)
-                1 -> TestSecondPage(navController)
+                1 -> TestSecondPage()
+                2 -> ReturnHomePage(navController)
             }
         }
         Box(
@@ -120,11 +129,71 @@ fun HRRPage(navController: NavController, maxHeartRate: Float, viewModel: HeartR
 }
 
 @Composable
-fun TestSecondPage(navController: NavController) {
+fun TestSecondPage() {
+
+    val entries = arrayListOf<Entry>()
+
+    for (i in 1..60) {
+        val x = 150 - i/2
+        if (i == 1 || i == 60 || i % 10 == 0) {
+            entries.add(Entry(i.toFloat(), x.toFloat()))
+        }
+    }
+
+    val dataset = LineDataSet(entries, "Heart Rate")
+    dataset.setDrawValues(false)
+
+    val labels = arrayListOf<String>()
+    labels.add("0s")
+    labels.add("60s")
+
+    val lineData = LineData(dataset)
+
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Second Page")
+        AndroidView({ context ->
+            LineChart(context).apply {
+                data = lineData
+                xAxis.position = XAxis.XAxisPosition.BOTTOM
+                xAxis.textColor = android.graphics.Color.WHITE
+                xAxis.valueFormatter = object : ValueFormatter() {
+                    override fun getFormattedValue(value: Float): String {
+                        return when (value) {
+                            1f -> "0s"
+                            60f -> "60s"
+                            else -> ""
+                        }
+                    }
+                }
+                xAxis.setDrawLabels(true)
+                xAxis.setDrawAxisLine(false)
+                xAxis.setDrawGridLines(false)
+                axisLeft.textColor = android.graphics.Color.WHITE
+                axisRight.textColor = android.graphics.Color.WHITE
+                legend.isEnabled = false
+                description.isEnabled = false
+
+//                description.text = "# of times Alice called Bob"
+//                description.textColor = android.graphics.Color.WHITE
+                animateX(1000)
+            }
+        }, modifier = Modifier
+            .fillMaxWidth(0.9f)
+            .height(150.dp)
+            .padding(16.dp))
+    }
+}
+
+@Composable
+fun ReturnHomePage(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Third Page")
         Button(onClick = { navController.navigate("landing_page") }, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF9CF2F9))) {
             Text(text = "Return to home page")
         }
